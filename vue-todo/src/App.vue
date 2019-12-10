67214a8db51e7ec9411c0d9099d4e835d8a12b65
Kidding Just <rlarkdlr@gmail.com>
2019-12-10 21:38:10 +0900

    vue refactoring

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput v-on:addItem="addOneItem"></TodoInput>
+    <!-- 하위 컴포넌트에서 발생시킨 이벤트 이름="현재 컴포넌트의 메서드" -->
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <!-- 내려보낼 프롭스 속성 이름 = "현재 컴포넌트 데이터 속성" -->
     <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
-    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
@@ -13,63 +15,82 @@ import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
+//로컬 컴포넌트를 설정하는 경우에는
+//new Vue에서 components 속성에서 설정해주었음. 
+
 export default {
-  data: function() {
+  data: function(){
     return {
       todoItems: []
     }
   },
   methods: {
-    addOneItem: function(todoItem) {
-      const obj = {completed: false, item: todoItem};
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      this.todoItems.push(obj);
+    addOneItem: function(todoItem){
+        var obj = {completed: false, item: todoItem};
+        // 저장하는 로직. value 부분을 String으로 변환
+        localStorage.setItem(todoItem, JSON.stringify(obj));
+        //화면에 있는 할일 목록까지 동기화. 배열에서 맨 끝에 하나의 값 넣는 것. 
+        this.todoItems.push(obj);
     },
-    removeOneItem: function(todoItem, index) {
-      this.todoItems.splice(index, 1);
+    removeOneItem: function(todoItem, index){
+      //예제에서는 key와 value가 동일. key를 넣어줌. 
       localStorage.removeItem(todoItem.item);
+      //localStorage 삭제 후 스크립트 화면에도 반영해주어야 함
+      //splice는 자바스크립트 배열 API. 특정 인덱스에서 1개를 지우겠다는 것. 
+      //변경 후 새로운 배열을 반환하는 api
+      this.todoItems.splice(index, 1);
     },
-    toggleOneItem: function(todoItem, index) {
-      todoItem.completed = !todoItem.completed;
+    toggleOneItem: function(todoItem, index){
+      // todoItem.completed = !todoItem.completed;
+      this.todoItems[index].completed = !this.todoItems[index].completed = !
+
+      //체크 내역을 localStorage에 저장.
+      //그런데 localStorage에 update 치는 API는 없음.
+      //그래서 삭제 후 다시 저장. 
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-    },
-    clearAllItems: function() {
-      this.todoItems = [];
-      localStorage.clear();
     }
   },
   created: function() {
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+    if (localStorage.length > 0){
+      for (var i=0; i<localStorage.length; i++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          //다시 JSON으로 변환.
+          //로컬 스토리지의 특성상 어쩔 수 없이 String전환 후 다시 Object로
+          //localStorage.key(i) 에서 item을 가져오고 이걸 다시 parsing 하겠다는 것. 
+          console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
           this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          // this.todoItems.push(localStorage.key(i))
         }
       }
     }
   },
   components: {
-    TodoHeader: TodoHeader,
-    TodoInput: TodoInput,
-    TodoList: TodoList,
-    TodoFooter: TodoFooter
-  }  
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter,
+  }
 }
+
 </script>
 
 <style>
-body {
+body{
   text-align: center;
-  background-color: #F6F6F8;
+  background-color: #F6F6F6;
 }
+
 input {
   border-style: groove;
   width: 200px;
 }
+
 button {
   border-style: groove;
 }
+
 .shadow {
-  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,79 +1,61 @@
 <template>
   <div class="inputBox shadow">
-    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
-    <span class="addContainer" v-on:click="addTodo">
-      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
-    </span>
-
-    <Modal v-if="showModal" @close="showModal = false">
-      <h3 slot="header">
-        경고 
-        <i class="closeModalBtn fa fa-times" 
-          aria-hidden="true" 
-          @click="showModal = false">
-        </i>
-      </h3>
-      <p slot="body">할 일을 입력하세요.</p>
-    </Modal>
+      <!-- v-model은 input에 입력된 텍스트를 동적으로 vue 인스턴스 안에 맵핑하는 역할 -->
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+      <!-- <button v-on:click="addTodo">add</button> -->
+      <span class="addContainer" v-on:click="addTodo">
+        <i class="fas fa-plus addBtn"></i>
+      </span>
   </div>
 </template>
 
 <script>
-import Modal from './common/Modal.vue'
-
 export default {
   data: function() {
     return {
-      newTodoItem: '',
-      showModal: false
+      newTodoItem: ""
     }
   },
   methods: {
     addTodo: function() {
-      if (this.newTodoItem !== '') {
-        const item = this.newTodoItem.trim();
-        this.$emit('addItem', item);
+      if (this.newTodoItem !== ''){
+        // emit('이벤트 이름', 인자1, 인자2, ...);
+        this.$emit('addTodoItem', this.newTodoItem);
+        // 이후에 다시 비워주기
         this.clearInput();
-      } else {
-        this.showModal = !this.showModal;
       }
     },
+    //텍스트 지우는 함수 
     clearInput: function() {
       this.newTodoItem = '';
     }
-  },
-  components: {
-    Modal: Modal
   }
-}
+}  
 </script>
 
-<style scoped>
-input:focus {
-  outline: none;
-}
-.inputBox {
-  background: white;
-  height: 50px;
-  line-height: 50px;
-  border-radius: 5px;
-}
-.inputBox input {
-  border-style: none;
-  font-size: 0.9rem;
-}
-.addContainer {
-  float: right;
-  background: linear-gradient(to right, #6478FB, #8763FB);
-  display: block;
-  width: 3rem;
-  border-radius: 0 5px 5px 0;
-}
-.addBtn {
-  color: white;
-  vertical-align: middle;
-}
-.closeModalBtn {
-  color: #42b983;
-}
+<style scope>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
